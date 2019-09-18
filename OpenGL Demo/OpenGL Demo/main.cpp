985dcdbdc1af50adc62beb73f0f7ca98d767fe74
Henry Oliver <henry983615@gmail.com>
2019-09-18 15:07:39 +1200

    Inital Commit

---
+++ OpenGL Demo/OpenGL Demo/main.cpp
@@ -0,0 +1,18 @@
+#include <glew.h>
+#include <freeglut.h>
+#include <SOIL.h>
+#include <fmod.hpp>
+#include <iostream>
+#include <time.h>
+#include <vector>
+#include <windows.h>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
